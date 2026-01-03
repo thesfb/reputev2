492dae309eab3602c176bfc4d5535c600c0a2b14
thesfb <codpulkit11@gmail.com>
2026-01-03 16:09:18 +0530

    Added minting

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // --- THIS IS THE MISSING PART ---
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        readline: false,
+      };
+    }
+    return config;
+  },
 }
 
 export default nextConfig
